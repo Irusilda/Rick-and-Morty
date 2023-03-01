3acbc5166c04f97c8b378653537f1b240694a99b
Irusilda <irina.lyubodeeva@yandex.ru>
2023-03-01 10:21:16 +0300

    edit splash screen (without additional activity)

--- app/src/main/java/com/example/rick_and_morty/MainActivity.kt
+++ app/src/main/java/com/example/rick_and_morty/MainActivity.kt
@@ -1,18 +1,37 @@
 package com.example.rick_and_morty
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+    private var keep = true
+    private val delay = 2000L
+    private val handler = Handler(Looper.getMainLooper())
+
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        Log.d("MyLog", "$splashScreen")
+        splashScreen.setKeepOnScreenCondition { keep }
+        handler.postDelayed({
+            keep = false
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.place_holder, MainFragment())
+                .commit()
+        }, delay)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("MyLog", "Main")
 
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.place_holder, MainFragment())
-            .commit()
+
+
+
     }
 }
