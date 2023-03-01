0cb7c83ab9733326742236d394ea10e278299caa
Irusilda <irina.lyubodeeva@yandex.ru>
2023-03-01 13:17:08 +0300

    add navigation

--- app/src/main/java/com/example/rick_and_morty/MainActivity.kt
+++ app/src/main/java/com/example/rick_and_morty/MainActivity.kt
@@ -4,34 +4,113 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
+import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.rick_and_morty.databinding.ActivityMainBinding
+import com.example.rick_and_morty.fragments.CharacterFragment
+import com.example.rick_and_morty.fragments.EpisodesFragment
+import com.example.rick_and_morty.fragments.LocationsFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Navigator {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var toolbar: Toolbar
+
+    private val currentFragment: Fragment
+        get() = supportFragmentManager.findFragmentById(R.id.place_holder)!!
+
+    private val fragmentListener = object : FragmentManager.FragmentLifecycleCallbacks() {
+        override fun onFragmentViewCreated(
+            fm: FragmentManager,
+            f: Fragment,
+            v: View,
+            savedInstanceState: Bundle?
+        ) {
+            super.onFragmentViewCreated(fm, f, v, savedInstanceState)
+            updateUi()
+        }
+    }
+
     private var keep = true
     private val delay = 2000L
     private val handler = Handler(Looper.getMainLooper())
 
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         val splashScreen = installSplashScreen()
-        Log.d("MyLog", "$splashScreen")
         splashScreen.setKeepOnScreenCondition { keep }
         handler.postDelayed({
             keep = false
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.place_holder, MainFragment())
-                .commit()
+            showCharacterFragment()
         }, delay)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.d("MyLog", "Main")
+        setContentView(binding.root)
 
+        toolbar = binding.root.findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-
-
+        binding.bottomNavigation.setOnItemSelectedListener { it ->
+            when (it.itemId) {
+                R.id.characters -> showCharacterFragment()
+                R.id.locations -> showLocationsFragment()
+                R.id.episodes -> showEpisodesFragment()
+            }
+            true
+        }
+        supportFragmentManager.registerFragmentLifecycleCallbacks(fragmentListener, false)
     }
+
+    private fun updateUi() {
+        val fragment = currentFragment
+        if (fragment is CustomTitle) {
+            toolbar.title = getString(fragment.getTitleRes())
+        } else toolbar.title = getString(R.string.app_name)
+    }
+
+    private fun launchFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.place_holder, fragment)
+            .commit()
+    }
+
+    override fun showCharacterFragment() {
+        launchFragment(CharacterFragment.newInstance())
+    }
+
+    override fun showLocationsFragment() {
+        launchFragment(LocationsFragment.newInstance())
+    }
+
+    override fun showEpisodesFragment() {
+        launchFragment(EpisodesFragment.newInstance())
+    }
+
+    override fun backPressed() {
+        val callBack = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (currentFragment is CharacterFragment )
+                    finish()
+                else {
+                    launchFragment(CharacterFragment.newInstance())
+                }
+            }
+        }
+        onBackPressedDispatcher.addCallback(this, callBack)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        supportFragmentManager.unregisterFragmentLifecycleCallbacks(fragmentListener)
+    }
+
 }
---
+++ app/src/main/java/com/example/rick_and_morty/Navigator.kt
@@ -0,0 +1,20 @@
+package com.example.rick_and_morty
+
+import androidx.fragment.app.Fragment
+
+fun Fragment.navigator(): Navigator {
+    return requireActivity() as Navigator
+}
+
+interface Navigator {
+
+    fun showCharacterFragment()
+
+    fun showLocationsFragment()
+
+    fun showEpisodesFragment()
+
+    fun backPressed()
+
+
+}
